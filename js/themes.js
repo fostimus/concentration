e64d036fbe47dbc334c1b1a69ee3155edd48c494
Derek Foster <derekfoster94@gmail.com>
2020-10-08 11:17:40 -0700

    added to themes, explicitly set text color on buttons

--- js/themes.js
+++ js/themes.js
@@ -23,24 +23,24 @@ const themes = [
   },
   //theme 3
   {
-    bgColor: "#1c0f13",
-    textColor: "#ebebeb",
-    modalBgColor: "#426a5a",
-    modalBorderColor: "#582b11",
-    btnBgColor: "#9882ac",
-    btnHoverColor: "#ebebeb",
-    btnBorderColor: "#ebebeb",
+    bgColor: "#B0C0BC",
+    textColor: "#797270",
+    modalBgColor: "#453F3C",
+    modalBorderColor: "#C1EDCC",
+    btnBgColor: "#C1EDCC",
+    btnHoverColor: "#797270",
+    btnBorderColor: "#453F3C",
     font: '"Junge", sans-serif'
   },
   //theme 4
   {
-    bgColor: "#1c0f13",
-    textColor: "#ebebeb",
-    modalBgColor: "#426a5a",
-    modalBorderColor: "#582b11",
-    btnBgColor: "#9882ac",
-    btnHoverColor: "#ebebeb",
-    btnBorderColor: "#ebebeb",
+    bgColor: "#D00000",
+    textColor: "#A2AEBB",
+    modalBgColor: "#3F88C5",
+    modalBorderColor: "#FFBA08",
+    btnBgColor: "#1C3144",
+    btnHoverColor: "#A2AEBB",
+    btnBorderColor: "#1C3144",
     font: '"Junge", sans-serif'
   },
   //theme 5
